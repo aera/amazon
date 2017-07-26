f3fb60bb0d4bd00a31d4ab27746e7b71b1ce11a3
aera <allain_era@yahoo.com>
2017-07-26 15:26:10 -0700

    tags implemented

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,9 @@ Rails.application.routes.draw do
     resources :reviews do
       resources :likes
     end
-
   end
+
+  resources :tags, only: [:index, :show]
 
   resources :reviews do
     resources :likes
